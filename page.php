da9932ffa28e432c63a29e58ed73b442943cab3e
SaphireVert <jerome.cosandey@gmail.com>
2023-03-15 14:11:37 +0100

    feat: render plugin page

---
+++ page.php
@@ -0,0 +1,16 @@
+<h1>WP plugin LastPageChanges</h1>
+<div>
+    Donne le nom du dernier utilisateur ayant modifié une page ainsi que la date
+</div>
+<div>
+    Version du plugin
+</div>
+<div>
+    Lien vers le code source du plugin
+</div>
+<div>
+    List des 5 pages modifiées le plus récemment avec l'information de la personne
+</div>
+<div>
+    La recherche de page / et / ou un drop down de toutes les pages, permettant d'obtenir cette information
+</div>
